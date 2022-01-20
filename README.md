68801eb59325a998637bcb8ec717dfa7508ac518
Eric Duhamel <ericxdu23@gmail.com>
2022-01-20 12:59:20 -0800

    import module and readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Table Scraper
+
+a Python module to scrape information from HTML tables.
+
+## Requirements
+
+    pip install beautifulsoup
+    pip install requests
+
+## Usage
+
+Table Scraper can download the HTML from any URL and save it for later, 
+so you don't have to query the website every time you parse the file.
+
+- python3 scraper.py --url="https://a.website.url/page_name --save-html=page.html
+
+To do anything with the data, the page must have at least one <table> 
+tag and you must provide a table number and a column number to lookup.
+
+- python3 scraper.py --html=page.html --table=0 --column=1 --print-data
+
+You can save the table data to a file with one line per field found.
+
+- python3 scraper.py --html=page.html --table=1 --column=0 --save-data=filename.txt
---
+++ scraper.py
@@ -0,0 +1,69 @@
+import re
+import requests
+import sys
+
+from bs4 import BeautifulSoup
+
+if len(sys.argv) < 2:
+    print("usage:")
+    print("  --url=<url>            download page at <url>")
+    print("  --html=<filename>      use <filename> as page")
+    print("  --table=<n>            select the <n>th table in the page")
+    print("  --column=<n>           select the <n>th column in the table")
+    print("  --print-data           print the text of each field selected")
+    print("  --save-data=<filename> save the text of each field to <filename>")
+    print("  --save-html=<filename> save downloaded page as <filename>")
+
+def main():
+    for arg in sys.argv:
+        if arg[0:9] == "--column=":
+            cnum = int(arg[9:])
+        elif arg[0:7] == "--html=":
+            filename = arg[7:]
+            with open(filename) as file:
+                text = file.read()
+        elif arg == "--print-data":
+            for line in get_data(text, tnum, cnum):
+                print(line)
+        elif arg[0:12] == "--save-data=":
+            filename = arg[12:]
+            with open(filename, "w") as file:
+                for line in get_data(text, tnum, cnum):
+                    file.write(line + "\n")
+        elif arg[0:12] == "--save-html=":
+            filename = arg[12:]
+            soup = BeautifulSoup(text, 'html.parser')
+            with open(filename, "w") as file:
+                file.write(soup.prettify())
+        elif arg[0:8] == "--table=":
+            tnum = int(arg[8:])
+        elif arg[0:6] == "--url=":
+            url = arg[6:]
+            r = requests.get(url)
+            text = r.text
+
+def get_data(text, tnum, cnum, patterns=(r'\[[^\]]*\]',)):
+    datum = []
+    soup = BeautifulSoup(text, 'html.parser')
+    for i, table in enumerate(soup.find_all('table')):
+        if i == tnum:
+            for trow in table.find_all('tr'):
+                for k, tdata in enumerate(trow.find_all('td')):
+                    if k == cnum:
+                        cat = ""
+                        for string in tdata.strings:
+                            string = string.replace("\"", "")
+                            string = string.replace("'", "")
+                            string = string.replace("(", "")
+                            string = string.replace(")", "")
+                            string = string.replace("\n", "")
+                            for pattern in patterns:
+                                string = re.sub(pattern, '', string)
+                            cat = cat + string
+                        datum.append(cat)
+    return datum
+
+if __name__ == "__main__":
+    try: main()
+    except KeyboardInterrupt: print("Keyboard Interrupt (Control-C)...")
+    sys.exit()
